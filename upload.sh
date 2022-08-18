f3554df7fdc6ca9b416fb5938cb94301fe90b351
K Smith <krsjunk@netscape.net>
2022-08-18 12:55:08 -0400

    fixed the exclude list of the tar command

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # "particle library upload" is giving a network error, this works...
-tar -zcvf library.tar.gz --exclude=".git" --exclude=".idea" --exclude="library.tar.gz" .
+tar -zcvf library.tar.gz --exclude=".git" --exclude=".idea" --exclude="library.tar.gz" --exclude="build" --exclude="target" --exclude="*.bin" .
 curl -F "archive=@library.tar.gz" "https://api.particle.io/v1/libraries?access_token=$1"
